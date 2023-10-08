859c848311a134c5bf0441fd6b577632730ea5e6
Faiznurfaza <faiznurfaza11@gmail.com>
2023-10-08 22:50:42 +0700

    style: fix mismatch UI on cart details in dark and light mode

--- src/components/pagination/paginations.styles.ts
+++ src/components/pagination/paginations.styles.ts
@@ -1,36 +1,33 @@
-import { useMemo } from "react";
 import { useDarkMode } from "@/utils/use-darkmode";
 
 export function PaginationStyles(page: number, maxPage: number) {
     const { isDarkMode } = useDarkMode()
-    return useMemo(() => {
-        const baseStyle = {
-            Container: "flex flex-col lg:flex-row items-center justify-between mt-4",
-            Text: "flex text-sm mb-2 ml-2 lg:mb-0",
-            ButtonContainer: "flex space-y-2 lg:space-y-0 lg:space-x-2 items-center",
-            Button: "px-4 py-2",
-            Icon: "h-4 w-4",
-            Disabled: "opacity-50 cursor-not-allowed",
-            Dark: {
-                Container: "bg-gray-800",
-                Text: "text-gray-200",
-                Button: "text-blue-400",
-            },
-            Light: {
-                Text: "text-gray-600",
-                Button: "text-blue-500",
-            },
-        };
+    const baseStyle = {
+        Container: "flex flex-col lg:flex-row items-center justify-between mt-4",
+        Text: "flex text-sm mb-2 ml-2 lg:mb-0",
+        ButtonContainer: "flex space-y-2 lg:space-y-0 lg:space-x-2 items-center",
+        Button: "px-4 py-2",
+        Icon: "h-4 w-4",
+        Disabled: "opacity-50 cursor-not-allowed",
+        Dark: {
+            Container: "bg-gray-800",
+            Text: "text-gray-200",
+            Button: "text-blue-400",
+        },
+        Light: {
+            Text: "text-gray-600",
+            Button: "text-blue-500",
+        },
+    };
 
-        const paginationStyles = {
-            Container: `${baseStyle.Container} ${isDarkMode ? baseStyle.Dark.Container : ''}`,
-            Text: `${baseStyle.Text} ${isDarkMode ? baseStyle.Dark.Text : baseStyle.Light.Text}`,
-            Icon: `${baseStyle.Icon}`,
-            ButtonContainer: `${baseStyle.ButtonContainer}`,
-            PrevButton: `${baseStyle.Button} ${isDarkMode ? baseStyle.Dark.Button : baseStyle.Light.Button} ${page === 1 ? baseStyle.Disabled : ''}`,
-            NextButton: `${baseStyle.Button} ${isDarkMode ? baseStyle.Dark.Button : baseStyle.Light.Button} ${page === maxPage ? baseStyle.Disabled : ''}`,
-        };
+    const paginationStyles = {
+        Container: `${baseStyle.Container} ${isDarkMode ? baseStyle.Dark.Container : ''}`,
+        Text: `${baseStyle.Text} ${isDarkMode ? baseStyle.Dark.Text : baseStyle.Light.Text}`,
+        Icon: `${baseStyle.Icon}`,
+        ButtonContainer: `${baseStyle.ButtonContainer}`,
+        PrevButton: `${baseStyle.Button} ${isDarkMode ? baseStyle.Dark.Button : baseStyle.Light.Button} ${page === 1 ? baseStyle.Disabled : ''}`,
+        NextButton: `${baseStyle.Button} ${isDarkMode ? baseStyle.Dark.Button : baseStyle.Light.Button} ${page === maxPage ? baseStyle.Disabled : ''}`,
+    };
 
-        return paginationStyles;
-    }, [isDarkMode, page, maxPage]);
+    return paginationStyles;
 }
--- src/modules/carts/styles/cart-details.styles.ts
+++ src/modules/carts/styles/cart-details.styles.ts
@@ -1,19 +1,12 @@
-import React, { useMemo } from 'react'
 import { useDarkMode } from '@/utils/use-darkmode'
 
 export function CartDetailsStyles() {
     const { isDarkMode } = useDarkMode()
-    return useMemo(() => {
-        const cardStyles: {
-            Content: string | undefined;
-            Head: React.CSSProperties | undefined;
-            Body: React.CSSProperties | undefined;
-        }= {
-            Content: isDarkMode ? "bg-gray-900" : "bg-gray-100",
-            Head: isDarkMode ? {color: "white"} : undefined,
-            Body: isDarkMode ? {width:"450px", color: "white"} : undefined,
-        };
+    const cardStyles = {
+        Content: isDarkMode ? "bg-gray-900" : "bg-gray-100",
+        Head: isDarkMode ? { color: "white" } : undefined,
+        Body: isDarkMode ? { width: "450px", color: "white" } : { width: "450px"},
+    };
 
-        return cardStyles
-    }, [isDarkMode])
+    return cardStyles;
 }
